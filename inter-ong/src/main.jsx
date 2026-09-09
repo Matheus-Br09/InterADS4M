f3abe8254b30cc3a6b93e74aeb9b74f15f8ba0fb
Matheus-Br09 <matheusps3.br06@gmail.com>
2026-09-08 22:36:10 -0300

    algumas alterações no front pra mostrar como funciona o react (Vai ter muitas mudanças) - 08/09

--- inter-ong/src/main.jsx
+++ inter-ong/src/main.jsx
@@ -2,9 +2,67 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+// Importa as páginas
+
+import LandingPage from './pages/LandingPage.jsx'
+import Doar from './pages/Doar.jsx'
+import Sobre from './pages/Sobre.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         path: "landing",
+//         element: <LandingPage />
+//       },
+//       {
+//         path: "Sobre",
+//         element: <Sobre />
+//       },
+//       {
+//         path: "Doar",
+//         element: <Doar />
+//       }
+//     ]
+//   },
+// ])
+
+
+// Jás aqui a rota das páginas para acessá-las, caso queira adicionar uma página, coloque-a aqui 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorPage />,
+    element: <App />,
+    children: [
+      {
+        index: true,
+        path: "landing",
+        element: <LandingPage />
+      },
+      {
+        path: "doar",
+        element: <Doar />
+      },
+      {
+        path: "sobre",
+        element: <Sobre />
+      }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ inter-ong/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route, Link  } from 'react-router-dom'
+import Sobre from './Sobre'
+import Doar from './Doar'
+
+export default function(){
+    return(
+        <div>
+            <h1 className='bg-rose-300'>Landing Page</h1>
+      
+        </div>
+    )
+}
