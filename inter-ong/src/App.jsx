f84a9b30be12cf8cbf19e5802795a087c9db3639
Matheus-Br09 <matheusps3.br06@gmail.com>
2026-09-09 20:54:05 -0300

    navbar feia porém está lá, falta embelezar e colocar o restante das páginas - 09/09

--- inter-ong/src/App.jsx
+++ inter-ong/src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Link, Outlet  } from 'react-router-dom'
+import { Link, Outlet  } from 'react-router-dom'
 
 import './App.css'
+
+import NavBar from './components/NavBar'
 
 
 function App() {
     return(
       <div className='bg-amber-500'>
-        <h1>Pagina inicial</h1>
-        <Outlet />
-        <p>Footer</p>
+
+        <NavBar />
+
+        <main>
+          <h1 className='text-center text-3xl'>Pagina inicial</h1>
+          <Outlet />
+        </main>
+        
+        <div className='text-center bottom-0'>
+          <p>Footer</p>
+        </div>
       </div>
     )
 }
