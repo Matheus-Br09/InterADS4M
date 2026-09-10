890aa5e846ca949941ae24940019dd1d872bcc9c
Matheus-Br09 <matheusps3.br06@gmail.com>
2026-09-09 21:47:44 -0300

    .

--- inter-ong/src/App.jsx
+++ inter-ong/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link, Outlet  } from 'react-router-dom'
 
 import './App.css'
+import NavBar from './components/NavBar'
 
 
 function App() {
     return(
       <div className='bg-amber-500'>
+        <NavBar />
+
         <h1>Pagina inicial</h1>
         <Outlet />
         <p>Footer</p>
