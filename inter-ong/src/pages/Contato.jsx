26129a58186d0d2445c80a748b29dfe068015a04
Matheus-Br09 <matheusps3.br06@gmail.com>
2026-09-13 16:12:09 -0300

    footer mais bonito - 13/09

--- inter-ong/src/pages/Contato.jsx
+++ inter-ong/src/pages/Contato.jsx
@@ -4,7 +4,7 @@ export default function Contato(){
     return (
         <div className="flex items-center justify-center">
             <main className="flex w-full h-156 items-center justify-center m-6 bg-rose-400 rounded-2xl gap-45">
-                <div className="bg-gray-100 rounded-2xl m-4 p-2.5">
+                <div className="bg-gray-100 rounded-2xl m-4 p-2.5 shadow-sm shadow-gray-500">
                     <div className="info-contact">
                         <p className="info-name">imeiu da impresa</p>
                         <p>caceta@gmail.com</p>
@@ -29,7 +29,7 @@ export default function Contato(){
                     
                 </div> 
                 <div>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.5527711809514!2d-34.83390742411175!3d-7.941686879106385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab3d11fee5da57%3A0x7e7b6c1816d65ae6!2sSOS%20TUDO%20PELO%20SOCIAL!5e0!3m2!1spt-BR!2sbr!4v1789083824825!5m2!1spt-BR!2sbr" frameborder="0" className="rounded-2xl size-125 m-5"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.5527711809514!2d-34.83390742411175!3d-7.941686879106385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab3d11fee5da57%3A0x7e7b6c1816d65ae6!2sSOS%20TUDO%20PELO%20SOCIAL!5e0!3m2!1spt-BR!2sbr!4v1789083824825!5m2!1spt-BR!2sbr" frameborder="0" className="rounded-2xl size-125 m-5 shadow-sm shadow-gray-500"></iframe>
                 </div>
                 
             </main>  
